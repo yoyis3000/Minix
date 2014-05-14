823f98b363c12b9bc3e1115bb0a85cad42ace0f3
george <georgebarrios@umail.ucsb.edu>
2014-05-13 23:11:08 -0700

    Getting bryces branch

---
+++ servers/sema/proto.h
@@ -0,0 +1,11 @@
+/* Function prototypes. */
+
+//struct schedproc;
+#include <timers.h>
+
+/* main.c */
+int main(void);
+//void setreply(int proc_nr, int result);
+
+/* schedule.c */
+int do_sema_init( 
